517e2ae2978b31b626ab2aaa00e31eda2814738c
MariPeshko <marunapeshko@gmail.com>
2025-09-26 15:40:05 +0200

    update

--- Level_00/exam_train/bigint.cpp
+++
@@ -1,6 +0,0 @@
-#include "bigint.hpp"
-
-std::ostream & operator<<(std::ostream &os, const bigint &b) {
-	os << b.num;
-	return os;
-};
--- Level_00/polyset/solution/set.hpp
+++
@@ -1,9 +0,0 @@
-/* 
-// Original: allows duplicates
-bag.insert(5);
-bag.insert(5);  // Now has: [5, 5]
-
-// Wrapped: prevents duplicates  
-set.insert(5);
-set.insert(5);  // Still has: [5] 
-*/
